ee3f93d54e8954bfbf388a27f425e3b2e27d999d
Michele Lee Lynch <michele.lynch8@yahoo.com>
2020-12-05 15:26:58 -0500

    when called table populates and styling of submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,3 @@
-var zipCode = {};
-var myJSON = JSON.stringify(zipCode);
-
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
-
-//app.appendChild(logo);
-//app.appendChild(container);
-
 //document.addEventListener('click', function();
   $("#submitbtn").click(function(){
     console.log("buttonWasClicked");
@@ -19,61 +10,52 @@ container.setAttribute('class', 'container');
           url: "https://data.pa.gov/resource/kp97-c26b.json?zipcode="+ zipCode + "&$limit=5&$$app_token=3DCZ6kR551km15CG5Wm0cxPvx",
           type: "GET",
         }).done(function(data) {
+          populateDataTable(data);
         alert("Retrieved " + data.length + " records from the dataset!");
         console.log(data);
         });
       }
   })
 
-//gets called by function name
-//takes in data as an argument
-//puts data in HTML table
+//table hidden until click
+$("#submitbtn").click(function(){
+  $("#myTable").toggle();
+});
+
+//function called - takes in data - displays in table
 function populateDataTable(data) {
   if(data.length > 0){
   	var Table = document.querySelector('#myTable');
-	Table.innerHTML = `<tr><th>Name</th><th>Zip Code</th><th>Lat</th><th>long</th></tr>`;
+	Table.innerHTML = `<tr><th>Name</th><th>Address</th><th>Phone Number</th><th>Lat</th><th>Long</th></tr>`;
     var table = document.getElementById("myTable");
+    console.log(table, "stop, start");
     for(var i=0; i<data.length; i++){
       var row = table.insertRow();
       var cell0 = row.insertCell(0);
       var cell1 = row.insertCell(1);
       var cell2 = row.insertCell(2);
       var cell3 = row.insertCell(3);
+      var cell4 = row.insertCell(4);
       cell0.innerHTML = data[i]["name"];
-      cell1.innerHTML = data[i]["zipcode"];
-      cell2.innerHTML = data[i]["lat"];
-      cell3.innerHTML = data[i]["lng"];
+      cell1.innerHTML = data[i]["address"];
+      cell2.innerHTML = data[i]["phone_no"];
+      //would like these hidden - need to use for Google Map
+      cell3.innerHTML = data[i]["lat"];
+      cell4.innerHTML = data[i]["lng"];
     }
   }
 }
 
+//google map
 
-
-// Storing data:
-myObj = {};
-myJSON = JSON.stringify();
-localStorage.setItem("");
+// Local Storage Storing data:
+//myObj = {};
+//myJSON = JSON.stringify();
+//localStorage.setItem("");
 // Retrieving data:
-text = localStorage.getItem("testJSON");
-obj = JSON.parse(text);
-document.getElementById("demo").innerHTML = obj.name;
-
-buildTable()
-
-function buildTable(data){
-  var table = document.getElementById('myZipCodeTable')
-
-  for (var i = 0; i < data.length; i++){
-    var row = `<tr>
-            <td>${data[i].program_type}</td>
-            <td>${data[i].name}</td>
-            <td>${data[i].address}</td>
-            <td>${data[i].phone_no}</td>
-            <td>${data[i].hours}</td>
-          </tr>`
-    table.innerHTML += row
-  }
-}
+//text = localStorage.getItem("testJSON");
+//obj = JSON.parse(text);
+//document.getElementById("demo").innerHTML = obj.name;
 
 /* Need to clear data */
 
